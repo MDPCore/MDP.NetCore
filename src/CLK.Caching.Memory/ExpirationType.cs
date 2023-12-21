0a592ccb6c14256115e30263b1b88a0973af65d5
clark159 <clark159@hotmail.com>
2023-12-21 19:23:08 +0800

    [src] 加入CLK.Caching.Memory

---
+++ src/CLK.Caching.Memory/ExpirationType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLK.Caching.Memory
+{
+    public enum ExpirationType
+    {
+        /// <summary>
+        /// 所有快取項目統一的失效時間
+        /// </summary>
+        UnifiedExpiration,
+
+        /// <summary>
+        /// 每個快取項目各自的失效時間
+        /// </summary>
+        IndividualExpiration
+    }
+}
