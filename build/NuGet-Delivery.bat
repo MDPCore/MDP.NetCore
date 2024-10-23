1f47707bb91869908c5da058337e1c7dac20698f
clark159 <clark159@hotmail.com>
2024-10-23 12:27:19 +0800

    [nuget] Release MDP.NetCore 8.0.7.46

--- build/NuGet-Delivery.bat
+++ build/NuGet-Delivery.bat
@@ -4,7 +4,7 @@ setlocal enabledelayedexpansion
 
 :: ================================================
 :: Variables
-set version=8.0.7.42
+set version=8.0.7.46
 
 set buildDir=%~dp0
 set srcDir=%~dp0..\src
