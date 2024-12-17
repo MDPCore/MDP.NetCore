40663f7c0d9342e044b7fe2c20498d45803fc1e6
clark159 <clark159@hotmail.com>
2024-12-17 15:01:22 +0800

    [src] MDP.Threading.Tasks: 加入此專案，提供手動管理Task流程的功能。

---
+++ src/MDP.Threading.Tasks.Lab/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MDP.Threading.Tasks.Lab
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // ManualTask
+            var manualTask = new ManualTask<int>();
+
+            // 模擬異步操作
+            Task.Run(() =>
+            {
+                Console.WriteLine("模擬工作開始...");
+                Thread.Sleep(1000);
+                Console.WriteLine("模擬工作完成，設置結果。");
+                manualTask.TrySetResult(123);
+                manualTask.TrySetResult(456);
+                //manualTask.TrySetException(new Exception("AAAAA"));
+                //manualTask.TrySetException(new Exception("BBBBB"));
+                //manualTask.Cancel();
+                //manualTask.Cancel();
+            });
+
+            // 等待結果
+            using (manualTask)
+            {
+                try
+                {
+                    var result = await manualTask.WaitAsync(TimeSpan.FromMilliseconds(2000));
+                    Console.WriteLine($"結果：{result}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Message: {ex.Message}");
+                }
+
+                try
+                {
+                    var result = await manualTask.WaitAsync(TimeSpan.FromMilliseconds(2000));
+                    Console.WriteLine($"結果：{result}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Message: {ex.Message}");
+                }
+            }
+
+            // End
+            Console.WriteLine("End...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/MDP.Threading.Tasks/ManualTask.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MDP.Threading.Tasks
+{
+    public class ManualTask : IDisposable
+    {
+        // Fields
+        private readonly TaskCompletionSource _completionSource = new TaskCompletionSource();
+
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+        private bool _disposed = false;
+
+
+        // Methods
+        public async Task WaitAsync()
+        {
+            // Execute
+            try
+            {
+                // WaitAsync
+                await _completionSource.Task.WaitAsync(_cancellationTokenSource.Token);
+            }
+            catch (Exception exception)
+            {
+                // TrySetException
+                _completionSource.TrySetException(exception);
+
+                // throw
+                throw;
+            }
+        }
+
+        public async Task WaitAsync(TimeSpan timeout)
+        {
+            // Execute
+            try
+            {
+                // WaitAsync
+                await _completionSource.Task.WaitAsync(timeout, _cancellationTokenSource.Token);
+            }
+            catch (Exception exception)
+            {
+                // TrySetException
+                _completionSource.TrySetException(exception);
+
+                // throw
+                throw;
+            }
+        }
+
+        public bool TrySetResult()
+        {
+            // TrySetResult
+            return _completionSource.TrySetResult();
+        }
+
+        public bool TrySetException(Exception exception)
+        {
+            // TrySetException
+            return _completionSource.TrySetException(exception);
+        }
+
+        public void Cancel()
+        {
+            // Sync
+            lock (_cancellationTokenSource)
+            {
+                // Require
+                if (_disposed == true) return;
+            }
+
+            // Cancel
+            _cancellationTokenSource.Cancel();
+        }
+
+        public void Dispose()
+        {
+            // Sync
+            lock (_cancellationTokenSource)
+            {
+                // Require
+                if (_disposed == true) return;
+                _disposed = true;
+            }
+
+            // Dispose
+            _cancellationTokenSource.Cancel();
+            _cancellationTokenSource.Dispose();
+        }
+    }
+
+    public class ManualTask<T> : IDisposable
+    {
+        // Fields
+        private readonly TaskCompletionSource<T> _completionSource = new TaskCompletionSource<T>();
+
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+        private bool _disposed = false;
+
+
+        // Methods
+        public async Task<T> WaitAsync()
+        {
+            // Execute
+            try
+            {
+                // WaitAsync
+                return await _completionSource.Task.WaitAsync(_cancellationTokenSource.Token);
+            }
+            catch (Exception exception)
+            {
+                // TrySetException
+                _completionSource.TrySetException(exception);
+
+                // throw
+                throw;
+            }
+        }
+
+        public async Task<T> WaitAsync(TimeSpan timeout)
+        {
+            // Execute
+            try
+            {
+                // WaitAsync
+                return await _completionSource.Task.WaitAsync(timeout, _cancellationTokenSource.Token);
+            }
+            catch (Exception exception)
+            {
+                // TrySetException
+                _completionSource.TrySetException(exception);
+
+                // throw
+                throw;
+            }          
+        }
+
+        public bool TrySetResult(T result)
+        {
+            // TrySetResult
+            return _completionSource.TrySetResult(result);
+        }
+
+        public bool TrySetException(Exception exception)
+        {
+            // TrySetException
+            return _completionSource.TrySetException(exception);
+        }
+
+        public void Cancel()
+        {
+            // Sync
+            lock (_cancellationTokenSource)
+            {
+                // Require
+                if (_disposed == true) return;
+            }
+
+            // Cancel
+            _cancellationTokenSource.Cancel();
+        }
+
+        public void Dispose()
+        {
+            // Sync
+            lock (_cancellationTokenSource)
+            {
+                // Require
+                if (_disposed == true) return;
+                _disposed = true;
+            }
+
+            // Dispose
+            _cancellationTokenSource.Cancel();
+            _cancellationTokenSource.Dispose();
+        }
+    }
+}
